2f289191b25006071895fdd918b24dcf33efcc63
Lucas Ismael <43651116+UchoaTI@users.noreply.github.com>
2020-06-06 03:38:24 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.use('/tmp', )
+app.use('/tmp', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.listen(3333);
